21b7f806fc836d8882ad3fe27fd685f445be95f4
Raj-Utsav <utsavraj911@outlook.com>
2024-11-04 12:17:23 +0530

    .

--- Binary_Tree.cpp/diameter.cpp
+++ Binary_Tree.cpp/diameter.cpp
@@ -42,6 +42,38 @@ BT_Node *to_BT(vector<int> &v)
     return root;
 }
 
+int height(BT_Node *root)
+{
+    if (!root)
+        return 0;
+    return 1 + max(height(root->left), height(root->right));
+}
+
+void diameter_brute(BT_Node *root, int *maxi)
+{
+    if (!root)
+        return;
+    int lh = height(root->left);
+    int rh = height(root->right);
+
+    *maxi = max(*maxi, lh + rh);
+
+    diameter_brute(root->left, maxi);
+
+    diameter_brute(root->right, maxi);
+}
+
+int diameter_optimize(BT_Node *root, int maxi){
+    if(!root) return 0;
+    int lHeight = diameter_optimize(root->left,maxi);
+    int rHeight = diameter_optimize(root->right,maxi);
+
+    maxi = max(maxi, lHeight+rHeight);
+
+    return max(lHeight, rHeight) + 1;
+    
+}
+
 int main()
 {
     {
@@ -50,9 +82,24 @@ int main()
 
     // todo In this block write your code
     {
-        vector<int> v{};
+        vector<int> v{1, 2, 3, 4, 5};
         BT_Node *root = to_BT(v);
-        
+
+        int maxi = 0;
+        diameter_brute(root, &maxi);
+        cout << "Diameter: " << maxi << endl;
+        maxi = diameter_optimize(root, maxi);
+        cout << "Diameter: " << maxi << endl;
+
+
+        // Additional test cases
+        vector<int> v2{1, 2, 3, -1, -1, 4, 5};
+        BT_Node *root2 = to_BT(v2);
+        maxi = 0;
+        diameter_brute(root2, &maxi);
+        cout << "Diameter: " << maxi << endl;
+        maxi = diameter_optimize(root2, maxi);
+        cout << "Diameter: " << maxi << endl;
     }
 
     return 0;
