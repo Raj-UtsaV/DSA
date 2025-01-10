d9d820b8aa9564837258db7c628a1198d194899e
Raj_Utsav <utsavraj911@outlook.com>
2025-01-10 10:35:58 +0530

    #

---
+++ Graph/DFS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+vector<int> dfsOfGraph(vector<vector<int>> &adj)
+{
+    
+}
+
+int main()
+{
+    vector<vector<int>> adj{{2, 3, 1}, {0}, {0, 4}, {0}, {2}};
+}
